65f6f700ef0f6db4b270eab5235886075134d945
ItsJustChad <chad@justchad.com>
2016-11-04 14:33:43 +0000

    Wrong file.

--- miraiattacks.py
+++ miraiattacks.py
@@ -1,14 +1,15 @@
 #@miraiattacks parser
 #modded this: http://www.craigaddyman.com/mining-all-tweets-with-python/
-#Author: ItsJustChad
-#Last updated: 11/2/2016
+#thanks to @japhypoker for the regex help
+#Author: @ItIsJustChad
+#Last Updated: 4 Nov 2016
 
 from twython import Twython
 import time
 import re
 import csv
 
-#get keys from apps.twitter.com
+#Get your API keys from https://apps.twitter.com/ 
 APP_KEY = ''
 APP_SECRET = ''
 OAUTH_TOKEN  = ''
@@ -16,11 +17,7 @@ OAUTH_SECRET = ''
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_SECRET)
 
-BOT_REGEX = re.compile(r'.*?-\s')
-TYPE_REGEX = re.compile(r'-\s.*flood')
-DURATION_REGEX = re.compile(r'\d+\Wseconds')
-TARGET_REGEX = re.compile(r'\d+\.\d+\.\d+\.\d+\/\d+')
-PORT_REGEX = re.compile(r'[P|p]ort:\s+\d+')
+REGEX = re.compile(r'^(.*?) - (.*?) for (\d+) seconds\s+\[Targets\]\s+(.*(?:(?!\n[Pp]ort:)\n.*)*)(?:\n[Pp]ort: (\d+))?')
 
 CSV_FILE = open("miraiattacks.csv","wb")
 TWEET_WRITER = csv.writer(CSV_FILE)
@@ -28,48 +25,54 @@ TWEET_WRITER = csv.writer(CSV_FILE)
 LATEST_TWEET = twitter.get_user_timeline(screen_name="miraiattacks",count=1)
 LATEST_TWEET = LATEST_TWEET[0]['id']
 ID_ARRAY = [LATEST_TWEET]
+LAST_ID = "0"
 
 for i in range(0, 16):
 	USER_TIMELINE = twitter.get_user_timeline(screen_name="miraiattacks",
 	count=200, include_retweets=False,max_id=ID_ARRAY[-1])
 	for tweet in USER_TIMELINE:
+
         	THE_TWEET = tweet['text'].encode('utf-8')
-		BOT = re.findall(BOT_REGEX,THE_TWEET)
-		if not BOT:
-			BOT = "None"
+
+		MATCHES = re.search(REGEX,THE_TWEET)
+
+		if MATCHES is None:
+			continue
+
+		if not MATCHES.group(1):
+			BOT = "Not found."
 		else:
-			BOT = BOT[0][:len(BOT[0])-3]
+			BOT = MATCHES.group(1)
 
-		TYPE = re.findall(TYPE_REGEX,THE_TWEET)
-		if not TYPE:
-			TYPE = "None"
+		if not MATCHES.group(2):
+			TYPE = "Not found."
 		else:
-			TYPE = TYPE[0][2:]
+			TYPE = MATCHES.group(2)
 
-		DURATION = re.findall(DURATION_REGEX,THE_TWEET)
-		if not DURATION:
-			DURATION = "None"
+		if not MATCHES.group(3):
+			DURATION = "Not found."
 		else:
-			DURATION = DURATION[0]
+			DURATION = MATCHES.group(3)
 
-		TARGETS = re.findall(TARGET_REGEX,THE_TWEET)
-		if not TARGETS:
-			TARGETS = "None"
+		if not MATCHES.group(4):
+                        TARGETS = "Not found."
+		else:
+			TARGETS = MATCHES.group(4)
 
-		PORT = re.findall(PORT_REGEX,THE_TWEET)
-		if not PORT:
-			PORT = "None"
-		else: 
-			PORT = PORT[0][6:]
+		if not MATCHES.group(5):
+                        PORT = "Not found."
+                else:
+                        PORT = MATCHES.group(5)
 
-		TWEET_WRITER.writerow((tweet['created_at'],tweet['id'],BOT,TYPE,DURATION,TARGETS,PORT))
+
+		TWEET_WRITER.writerow((tweet['created_at'],tweet['id'],BOT,TYPE,DURATION,str(TARGETS).strip('[]'),PORT))
 
 		print "Wrote: ", tweet['created_at'], tweet['id'], BOT, TYPE, DURATION, TARGETS, PORT
 
 	ID_ARRAY.append(tweet['id'])
-	if ID_ARRAY[len(ID_ARRAY)-1] == ID_ARRAY[len(ID_ARRAY)-2]:
+	if ID_ARRAY[len(ID_ARRAY)-2] == ID_ARRAY[len(ID_ARRAY)-1]:
 		break
-	else:
-		time.sleep(10)
+
+	time.sleep(10)
 
 CSV_FILE.close()
